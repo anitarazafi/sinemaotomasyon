21655bb2e60be4c414801d2708fc1b717bc95361
Anita Razafindrakotovao <anitarazafindrakotovao@gmail.com>
2023-06-07 17:29:22 +0300

    Add project files.

---
+++ SinemaOtomasyonu/Database.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinemaOtomasyonu
+{
+    public static class Database
+    {
+        public static SqlConnection SqlConnection;
+
+        public static string SqlConnectionString = ""; //TODO: add real string
+
+        public static bool connect()
+        {
+            bool result = false;
+
+            try
+            {
+                SqlConnection = new SqlConnection(SqlConnectionString);
+                SqlConnection.Open();
+                result = true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return result;
+        }
+
+        public static void disconnect()
+        {
+            if (SqlConnection.State == System.Data.ConnectionState.Open)
+            {
+                SqlConnection.Close();
+            }
+        }
+    }
+}
---
+++ SinemaOtomasyonu/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SinemaOtomasyonu
+{
+    public partial class AnaPencere : Form
+    {
+        public AnaPencere()
+        {
+            InitializeComponent();
+        }
+
+        private void AnaPencere_Load(object sender, EventArgs e)
+        {
+            /*
+            bool result = Database.connect();
+
+            if (!result)
+            {
+                MessageBox.Show("Bağlantı hatası!", "ex", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            */
+        }
+
+        private void AnaPencere_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            /*
+            Database.disconnect();*/
+        }
+
+        private void btn_salonlar_Click(object sender, EventArgs e)
+        {
+            Form_Salon form_Salon = new Form_Salon();
+            form_Salon.ShowDialog();
+        }
+    }
+}
