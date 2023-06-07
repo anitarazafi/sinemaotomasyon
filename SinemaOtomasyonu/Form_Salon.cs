e44846cb1d194327bd64eebcb74f1ebeb41e2c5f
Anita Razafindrakotovao <anitarazafindrakotovao@gmail.com>
2023-06-07 18:00:39 +0300

    List salonlar

--- SinemaOtomasyonu/Form_Salon.cs
+++ SinemaOtomasyonu/Form_Salon.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,46 @@ namespace SinemaOtomasyonu
         public Form_Salon()
         {
             InitializeComponent();
+        }
+
+        private void btn_salon_ekle_Click(object sender, EventArgs e)
+        {
+            string query = "INSERT INTO salonlar (numara) VALUES (@numara)";
+
+            SqlCommand cmd = new SqlCommand(query, Database.SqlConnection);
+
+            SqlParameter numaraParameter = new SqlParameter("@numara", System.Data.SqlDbType.VarChar); // Control this
+            numaraParameter.Value = txt_salonNumara.Text;
+            cmd.Parameters.Add(numaraParameter);
+
+            cmd.ExecuteNonQuery();
+        }
+
+        private void Form_Salon_Load(object sender, EventArgs e)
+        {
+            DataTable salonlar = salonlistele(); //control this
+
+            grd_salonlar.DataSource = salonlar;
+        }
+
+        public static DataTable salonlistele()
+        {
+            DataTable dt = new DataTable();
+
+            try
+            {
+                string query = "SELECT s.id AS 'Id', s.numara AS 'Numara' * FROM salonlar s";
+                SqlCommand cmd = new SqlCommand(query, Database.SqlConnection);
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+                adapter.Fill(dt);
+
+            } catch(Exception ex) {
+                throw ex;
+            }
+
+            return dt;
         }
     }
 }
